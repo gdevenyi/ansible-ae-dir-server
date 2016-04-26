3a88f6b66a45f1f293fbb0c390f48849c784c39b
Michael Stroeder <michael@stroeder.com>
2016-04-26 11:18:52 +0200

    corrected expected authz-DN

--- files/slapd_checkmk.sh
+++ files/slapd_checkmk.sh
@@ -3,4 +3,4 @@
 /usr/local/sbin/slapd_checkmk.py \
   'ldapi://%2Fusr%2Flocal%2Fopenldap%2Fvar%2Frun%2Fldapi' \
   "ldaps://$(hostname -f)" \
-  "dn:uid=system_slapd-repl-$(hostname),cn=ae,ou=ae-dir"
+  "dn:uid=system_slapd_$(hostname),cn=ae,ou=ae-dir"
