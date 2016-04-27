36b92090794893c8e2747b530eea891ef8894f3b
Michael Stroeder <michael@stroeder.com>
2016-04-27 11:57:57 +0200

    separate role slapd_check

--- files/slapd_checkmk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/usr/local/sbin/slapd_checkmk.py \
-  'ldapi://%2Fusr%2Flocal%2Fopenldap%2Fvar%2Frun%2Fldapi' \
-  "ldaps://$(hostname -f)" \
-  "dn:uid=system_slapd_$(hostname),cn=ae,ou=ae-dir"
