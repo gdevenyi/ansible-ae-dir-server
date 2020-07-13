cec6e840569d82de69e9f653bcfa1ffcb3a26270
David Arnold <dar@xoe.solutions>
2020-07-12 19:19:44 -0500

    Expand usage reports

--- defaults/gen_usage.sh
+++ defaults/gen_usage.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-rm ./USAGE_REPORT.md
-echo "$(
-  for f in ./main/main.yml; do
-    echo
-    echo "### \`$f\`"
-    echo
-    for p in $(yq r $f --printMode p "*" | sort); do
-        echo "#### \`$p\`:"
-        for r in $(cd .. && rg -l -g 'defaults/main/*.yml' "\{.* $p.*\}" | sort); do
-            if [[ $r != "defaults/main/main.yml" ]]; then
-                echo "  - variable interfaced in _\`$r\`_"
-            elif [[ $r == "defaults/main/main.yml" ]]; then
-                echo "  - self-referenced in _\`$r\`_"
-            else
-                echo "# AN ERROR OCCURRED"
-            fi
-        done
-        for r in $(cd .. && rg -l -g '!defaults/**' " $p" | sort); do
-            if [[ $r == *".yml" ]]; then
-                echo "  - ansible usage in \`$r\`"
-            else
-                echo "  - direct template usage in **\`$r\`**"
-            fi
-        done
+SUFFIX=.USAGE_REPORT.md
+cd ..
+for f in defaults/main/*.yml; do
+    rm $f$SUFFIX
+    echo "$(
+        for p in $(yq r $f --printMode p "*" | sort); do
+            echo "#### \`$p\`:"
+            for r in $(rg -l -g 'defaults/main/*.yml' "\{.* $p.*\}" | sort); do
+                if [[ $r != "$f" ]]; then
+                    echo "  - variable interfaced in _\`$r\`_"
+                elif [[ $r == "$f" ]]; then
+                    echo "  - self-referenced in _\`$r\`_"
+                else
+                    echo "# AN ERROR OCCURRED"
+                fi
+            done
+            for r in $(rg -l -g '!defaults/**' " $p" | sort); do
+                if [[ $r == *".yml" ]]; then
+                    echo "  - ansible usage in \`$r\`"
+                else
+                    echo "  - direct template usage in **\`$r\`**"
+                fi
+            done
     done
-  done
-)" > ./USAGE_REPORT.md
+    )" > $f$SUFFIX
+done
+cd defaults
